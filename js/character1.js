2e3de12a30258c1cac3925bfcbba6af522be3b6a
JuanCarlos407 <31823343+Juancarlos407@users.noreply.github.com>
2022-09-12 13:01:30 +0200

    Add files via upload

---
+++ js/character1.js
@@ -0,0 +1,113 @@
+const SCALE = 2; // escala del pj
+const WIDTH = 16; // píxeles para las casillas del sprite eje X
+const HEIGHT = 18; // píxeles para las casillas del sprite eje Y
+const SCALED_WIDTH = SCALE * WIDTH;
+const SCALED_HEIGHT = SCALE * HEIGHT;
+const CYCLE_LOOP = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; // bucle para el sprite del personaje eje X
+const FACING_DOWN = 0; // posición del sprite eje Y
+const FACING_UP = 1; // posición del sprite eje Y
+const FACING_LEFT = 2; // posición del sprite eje Y
+const FACING_RIGHT = 3; // posición del sprite eje Y
+const FRAME_LIMIT = 12;
+
+let MOVEMENT_SPEED = 1.2; // constante de velocidad de movimiento para el pj
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+let keyPresses = {};
+let currentDirection = FACING_DOWN; // posición inicial del sprite
+let currentLoopIndex = 0;
+let frameCount = 0;
+let positionX = 360; // posición inicial del pj dentro del mapa en px
+let positionY = 105; // posición inicial del pj dentro del mapa en px
+let img = new Image();
+
+function boost() {
+  MOVEMENT_SPEED = 4;
+}
+
+function boostOff() {
+  MOVEMENT_SPEED = 1.2;
+}
+
+document.addEventListener("keydown", event => {
+  if (event.isComposing || event.keyCode === 32) { // al apretar spacebar modificamos la variable MOVEMENT_SPEED
+    boost();
+  }
+  else if (event.isComposing || event.keyCode !== 32) { // al apretar spacebar modificamos la variable MOVEMENT_SPEED
+    boostOff();
+  }
+});
+
+
+window.addEventListener('keydown', keyDownListener);
+function keyDownListener(event) {
+    keyPresses[event.key] = true;
+}
+
+window.addEventListener('keyup', keyUpListener);
+function keyUpListener(event) {
+    keyPresses[event.key] = false;
+}
+
+function loadImage() {
+  img.src = "../img/pj.png"; // cargamos la img del pj
+  img.onload = function() {
+    window.requestAnimationFrame(gameLoop);
+  };
+}
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+                canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+}
+
+loadImage();
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // inicializar el canvas y la posición del mismo.
+
+  let hasMoved = false;
+
+  if (keyPresses.w) {
+    moveCharacter(0, -MOVEMENT_SPEED, FACING_UP); // movimiento arriba con w, (ejeX, ejeY, posición inicial) podemos modificar la velocidad.
+    hasMoved = true;
+  } else if (keyPresses.s) {
+    moveCharacter(0, MOVEMENT_SPEED, FACING_DOWN); // movimiento abajo con s
+    hasMoved = true;
+  } else if (keyPresses.a) {
+    moveCharacter(-MOVEMENT_SPEED*1.5, 0, FACING_LEFT); // movimiento izquierda con a
+    hasMoved = true;
+  } else if (keyPresses.d) {
+    moveCharacter(MOVEMENT_SPEED, 0, FACING_RIGHT); // movimiento derecha con d
+    hasMoved = true;
+  }
+
+  if (hasMoved) {
+    frameCount++;
+    if (frameCount >= FRAME_LIMIT) {
+      frameCount = 0;
+      currentLoopIndex++;
+      if (currentLoopIndex >= CYCLE_LOOP.length) {
+        currentLoopIndex = 0;
+      }
+    }
+  }
+
+  if (!hasMoved) {
+    currentLoopIndex = 0;
+  }
+
+  drawFrame(CYCLE_LOOP[currentLoopIndex], currentDirection, positionX, positionY);
+  window.requestAnimationFrame(gameLoop);
+}
+
+function moveCharacter(deltaX, deltaY, direction) {
+  if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
+    positionX += deltaX;
+  }
+  if (positionY + deltaY > 0 && positionY + SCALED_HEIGHT + deltaY < canvas.height) {
+    positionY += deltaY;
+  }
+  currentDirection = direction;
+}
